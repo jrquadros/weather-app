fc44c4d5f187577b98fc4d942da11205e686e290
Júnior Quadros <jrmquadros@gmail.com>
2020-09-26 00:42:31 -0300

    feat(redux): add store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { Store } from './redux/Store'
 import { SafeAreaView, View, Text } from 'react-native'
 
 export const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>App</Text>
-      </View>
-    </SafeAreaView>
+    <Provider store={Store}>
+      <SafeAreaView>
+        <View>
+          <Text>App</Text>
+        </View>
+      </SafeAreaView>
+    </Provider>
   )
 }
